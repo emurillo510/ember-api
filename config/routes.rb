38c23807fccfa44534e81e6a15976d9d642a9ceb
Elton Murillo <emurillo510@gmail.com>
2015-03-07 14:38:55 -0800

    Adds namespace :api

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+
+    namespace :api do
+       # Directs /admin/products/* to Admin::ProductsController
+       # (app/controllers/admin/products_controller.rb)
+    end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
