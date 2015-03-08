83e7764b7fa0019541b36bf672fb196df00812d0
Elton Murillo <emurillo510@gmail.com>
2015-03-08 14:09:32 -0700

    Adds generator for rspec

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
 
-  # Api definition
-  namespace :api, defaults: { format: :json },
-    scope module: :v1 do
-      # We are going to list our resources here
+    namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/' do
+       scope module: :v1 do
+
+       end
     end
-  end
 end
