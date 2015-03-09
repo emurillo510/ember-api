b56bb181b4fc7aef8e7133a1a0461873c919aced
Elton Murillo <emurillo510@gmail.com>
2015-03-08 18:08:57 -0700

    Adds resources for routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
     namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/' do
        scope module: :v1 do
 
+         resources :posts, :comments, :brands, :users
+
        end
     end
 end
